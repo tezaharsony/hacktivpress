19bbf07c20b5c2ca2472818e788ddb9c3737ea92
tezaharsony <teza.harsony230394@gmail.com>
2017-09-14 15:31:08 +0700

    get article belum

--- server/Controllers/articlesController.js
+++ server/Controllers/articlesController.js
@@ -1,73 +1,73 @@
 const mongoose = require('mongoose')
 const articleModel = require('../Models/articleModel')
-
+mongoose.connect('mongodb://localhost:27017/hackpress_tezaharsony')
 
 var getAll = function (req, res) {
-  articleModel.find({}, function (err,article){
+  articleModel.find({}, function (err,articles){
     if(!err) {
-      res.send(article)
+      res.send(articles)
     } else {
       res.send(err)
     }
   })
 }
 
-var getById = function (req, res) {
-  articleModel.findOne({_id : req.params.id }, function (err, article){
-    if(!err) {
-      res.send(article)
-    } else {
-      res.send(err)
-    }
-  })
-}
-
-var addArticle = function (req, res) {
-  var Article = new articleModel({
-    title: req.body.title,
-    author: req.body.author,
-    category: req.params.id,
-    description: req.body.description
-  })
-
-  Article.save(function (err,article) {
-    if (!err) {
-      res.send(article)
-    } else {
-      res.send(err)
-    }
-  })
-}
-
-var updateArticle = function (req, res){
-  articleModels.findByIdAndUpdate({
-    _id : req.params.id
-  },{
-    article : req.body.article
-  })
-  .then(article => {
-    res.send(article)
-  })
-  .catch(err =>{
-    res.send(err)
-  })
-}
-
-var removeArticle = function (req, res) {
-  articleModels.remove({_id: req.params.id}, function (err){
-    if (!err) {
-      res.send("data deleted")
-    } else {
-      res.send(err)
-    }
-  })
-}
+// var getById = function (req, res) {
+//   articleModel.findOne({_id : req.params.id }, function (err, articles){
+//     if(!err) {
+//       res.send(article)
+//     } else {
+//       res.send(err)
+//     }
+//   })
+// }
+//
+// var addArticle = function (req, res) {
+//   var Articles = new articleModel({
+//     title: req.body.title,
+//     author: req.body.author,
+//     category: req.params.id,
+//     description: req.body.description
+//   })
+//
+//   Article.save(function (err,articles) {
+//     if (!err) {
+//       res.send(articles)
+//     } else {
+//       res.send(err)
+//     }
+//   })
+// }
+//
+// var updateArticle = function (req, res){
+//   articleModels.findByIdAndUpdate({
+//     _id : req.params.id
+//   },{
+//     articles : req.body.articles
+//   })
+//   .then(articles => {
+//     res.send(article)
+//   })
+//   .catch(err =>{
+//     res.send(err)
+//   })
+// }
+//
+// var removeArticle = function (req, res) {
+//   articleModels.remove({_id: req.params.id}, function (err){
+//     if (!err) {
+//       res.send("data deleted")
+//     } else {
+//       res.send(err)
+//     }
+//   })
+// }
 
 
 module.exports = {
-  getAll,
-  getById,
-  addArticle,
-  updateArticle,
-  removeArticle
+  getAll
+  // getById,
+  // addArticle,
+  // updateArticle,
+  // removeArticle
 };
--- server/Models/articleModel.js
+++ server/Models/articleModel.js
@@ -1,16 +1,29 @@
+// var mongoose = require('mongoose')
+// var Schema = mongoose.Schema
+// mongoose.connect('mongodb://localhost:27017/hackpress_tezaharsony')
+//
+// const articlesSchema = new Schema ({
+//    title: String,
+//    description: String,
+//    author: String,
+//    category: String
+//
+// })
+//
+// const articles = mongoose.model('articles', articlesSchema)
+//
+// module.exports = articles;
+
+
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 mongoose.connect('mongodb://localhost:27017/hackpress_tezaharsony')
 
 const articleSchema = new Schema ({
    title: String,
-   description: String,
-   author: {
-     type: Schema.Types.ObjectId,
-     ref:'user'
-   }
-   category: String
-
+   author: String,
+   category: String,
+   description: String
 })
 
 const articles = mongoose.model('articles', articleSchema)
